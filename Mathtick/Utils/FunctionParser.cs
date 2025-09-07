16047635c7866977c9958e0f75e918c8af0d8c3e
bapti <baptisteluciani@outlook.com>
2025-09-07 21:13:36 +0200

    feat(parser): Function Parser

--- Mathtick/Utils/FunctionParser.cs
+++ Mathtick/Utils/FunctionParser.cs
@@ -1,4 +1,5 @@
-﻿using NCalc;
+﻿using Mathtick.Models;
+using NCalc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,12 +18,34 @@ namespace Mathtick.Utils
            
             expr = Regex.Replace(expr, @"(\d+(\.\d+)?|x)\s*\^\s*(\d+(\.\d+)?|x)", "Pow($1,$3)");
 
+         
+
             return (x) =>
             {
                 try
                 {
                     var e = new Expression(expr);
                     e.Parameters["x"] = x;
+
+                    e.EvaluateFunction += (name, args) =>
+                    {
+                        string func = name.ToLower();
+                        var mathFunc = MathFunctionLibrary.Functions.Find(f => f.Name == func);
+                        if (mathFunc != null)
+                        {
+                            if (args.Parameters.Length < mathFunc.ParameterCount)
+                            {
+                                args.Result = double.NaN;
+                                return;
+                            }
+
+                            var paramValues = new double[mathFunc.ParameterCount];
+                            for (int i = 0; i < mathFunc.ParameterCount; i++)
+                                paramValues[i] = Convert.ToDouble(args.Parameters[i].Evaluate());
+
+                            args.Result = mathFunc.Func(paramValues);
+                        }
+                    };
                     var result = e.Evaluate();
                     return Convert.ToDouble(result);
                 }
