05461441a3daa96d7cb248da58b571415d15ea1b
bapti <baptisteluciani@outlook.com>
2025-09-07 21:14:17 +0200

    feat(utils): Color To Bruch for viewModel

---
+++ Mathtick/Utils/ColorToBruchConverter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+
+namespace Mathtick.Utils
+{
+    public class ColorToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is Color c)
+                return new SolidColorBrush(c);
+            return new SolidColorBrush(Colors.Transparent);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value is SolidColorBrush brush)
+                return brush.Color;
+            return Colors.Transparent;
+        }
+    }
+}
