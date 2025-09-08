7320e412e6e99cbff0a324cffe1c01a6d64fee64
bapti <baptisteluciani@outlook.com>
2025-09-08 21:40:13 +0200

    feat(utils): graph executor & serializer was created with modifications.

---
+++ Mathtick/Utils/Exec/GraphExecutor.cs
@@ -0,0 +1,92 @@
+﻿using Mathtick.Models.IA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathtick.Utils.Exec
+{
+    public sealed class GraphExecutor
+    {
+        private readonly GraphModel _graph;
+        public GraphExecutor(GraphModel graph) => _graph = graph;
+
+        public async Task<Dictionary<string, object?>> RunAsync(params string[] targetNodeIds)
+            => await Task.Run(() => RunInternal(targetNodeIds));
+
+        private Dictionary<string, object?> RunInternal(string[] targets)
+        {
+            var order = TopologicalOrder(); 
+            var valueCache = new Dictionary<string, object?>(); 
+            var portCache = new Dictionary<(string nodeId, string portName), object?>();
+
+            foreach (var node in order)
+            {
+
+                var inputs = new Dictionary<string, object?>();
+                foreach (var pIn in node.Inputs)
+                {
+                    var edge = _graph.Edges.FirstOrDefault(e => e.ToNodeId == node.Id && e.ToPortId == pIn.Id);
+                    if (edge == null) continue; 
+                    var fromNode = _graph.GetNode(edge.FromNodeId)!;
+                    var fromPort = fromNode.Outputs.First(po => po.Id == edge.FromPortId);
+                    var key = (fromNode.Id, fromPort.Name);
+                    if (portCache.TryGetValue(key, out var v)) inputs[pIn.Name] = v;
+                }
+
+                var result = node.Compute(inputs);
+
+                if (node.Outputs.Count <= 1)
+                {
+                    valueCache[node.Id] = result;
+                    if (node.Outputs.Count == 1)
+                        portCache[(node.Id, node.Outputs[0].Name)] = result;
+                }
+                else
+                {
+                    if (result is Dictionary<string, object?> rd)
+                    {
+                        foreach (var kv in rd)
+                            portCache[(node.Id, kv.Key)] = kv.Value;
+                    }
+                    valueCache[node.Id] = result;
+                }
+            }
+
+            var outDict = new Dictionary<string, object?>();
+            foreach (var id in targets)
+                outDict[id] = valueCache.TryGetValue(id, out var v) ? v : null;
+
+            return outDict;
+        }
+
+        private List<NodeBase> TopologicalOrder()
+        {
+            var indeg = _graph.Nodes.ToDictionary(n => n.Id, _ => 0);
+            foreach (var e in _graph.Edges)
+            {
+                if (indeg.ContainsKey(e.ToNodeId)) indeg[e.ToNodeId]++;
+            }
+
+            var q = new Queue<NodeBase>(_graph.Nodes.Where(n => indeg[n.Id] == 0));
+            var order = new List<NodeBase>();
+
+            while (q.Count > 0)
+            {
+                var n = q.Dequeue();
+                order.Add(n);
+                foreach (var e in _graph.Edges.Where(x => x.FromNodeId == n.Id))
+                {
+                    indeg[e.ToNodeId]--;
+                    if (indeg[e.ToNodeId] == 0)
+                        q.Enqueue(_graph.GetNode(e.ToNodeId)!);
+                }
+            }
+            if (order.Count != _graph.Nodes.Count)
+                throw new InvalidOperationException("Graph contains a cycle");
+
+            return order;
+        }
+    }
+}
---
+++ Mathtick/Utils/IO/GraphSerializer.cs
@@ -0,0 +1,88 @@
+﻿using Mathtick.Models.IA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Mathtick.Utils.IO
+{
+    public static class GraphSerializer
+    {
+        private static readonly JsonSerializerOptions _opt = new()
+        {
+            WriteIndented = true,
+            Converters = { new NodePolymorphicConverter() }
+        };
+
+        public static string ToJson(GraphModel g) => JsonSerializer.Serialize(g, _opt);
+        public static GraphModel FromJson(string json) => JsonSerializer.Deserialize<GraphModel>(json, _opt)!;
+    }
+
+    public sealed class NodePolymorphicConverter : JsonConverter<NodeBase>
+    {
+        public override NodeBase? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            using var doc = JsonDocument.ParseValue(ref reader);
+            var root = doc.RootElement;
+
+            if (!root.TryGetProperty("Title", out var titleProp)) return null!;
+            var title = titleProp.GetString() ?? "";
+
+            NodeBase node = title switch
+            {
+                "Input" => new Mathtick.Models.IA.InputNode(),
+                "Constant" => new Mathtick.Models.IA.ConstantNode(),
+                "Add" => new Mathtick.Models.IA.AddNode(),
+                "Multiply" => new Mathtick.Models.IA.MulNode(),
+                "Sigmoid" => new Mathtick.Models.IA.SigmoidNode(),
+                "Dense" => new Mathtick.Models.IA.DenseNode(),
+                _ => throw new NotSupportedException($"Unknown node type: {title}")
+            };
+
+            var json = root.GetRawText();
+            var clone = JsonSerializer.Deserialize<NodeClone>(json)!;
+
+            node.Id = clone.Id;
+            node.Title = clone.Title;
+            node.X = clone.X; node.Y = clone.Y;
+
+            node.Inputs.Clear(); node.Outputs.Clear();
+            foreach (var p in clone.Inputs) node.Inputs.Add(p);
+            foreach (var p in clone.Outputs) node.Outputs.Add(p);
+
+            foreach (var kv in clone.Params)
+                node.Params[kv.Key] = kv.Value!;
+
+            return node;
+        }
+
+        public override void Write(Utf8JsonWriter writer, NodeBase value, JsonSerializerOptions options)
+        {
+            var clone = new NodeClone
+            {
+                Id = value.Id,
+                Title = value.Title,
+                X = value.X,
+                Y = value.Y,
+                Inputs = value.Inputs,
+                Outputs = value.Outputs,
+                Params = value.Params
+            };
+            JsonSerializer.Serialize(writer, clone, options);
+        }
+
+        private sealed class NodeClone
+        {
+            public string Id { get; set; } = "";
+            public string Title { get; set; } = "";
+            public float X { get; set; }
+            public float Y { get; set; }
+            public List<Mathtick.Models.IA.Port> Inputs { get; set; } = new();
+            public List<Mathtick.Models.IA.Port> Outputs { get; set; } = new();
+            public Dictionary<string, object?> Params { get; set; } = new();
+        }
+    }
+}
